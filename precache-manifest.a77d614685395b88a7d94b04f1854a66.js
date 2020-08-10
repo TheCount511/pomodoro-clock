f44e18fed9bc0d57b4dd25988692fb8a6157a701
Fadhil <fadhil.akins@gmail.com>
2020-08-10 14:42:21 +0100

    Updates

---
+++ precache-manifest.a77d614685395b88a7d94b04f1854a66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62d068bfc52c8301cf976c43286aca8d",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "5cdf012ccc6a9ee31b58",
+    "url": "/pomodoro-clock/static/css/main.718f3c51.chunk.css"
+  },
+  {
+    "revision": "5396c5a0b2219171fe3c",
+    "url": "/pomodoro-clock/static/js/2.b6c545bc.chunk.js"
+  },
+  {
+    "revision": "596e4823497e45004d2db4c50beba3f3",
+    "url": "/pomodoro-clock/static/js/2.b6c545bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cdf012ccc6a9ee31b58",
+    "url": "/pomodoro-clock/static/js/main.d37963b7.chunk.js"
+  },
+  {
+    "revision": "d420e891230cef74bfe7",
+    "url": "/pomodoro-clock/static/js/runtime-main.cd73fa2f.js"
+  }
+]);
